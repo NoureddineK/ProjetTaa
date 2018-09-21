79028f2710a317e79605fba2aeb36ca7a62b9831
NoureddineK <noureddine.kadri05@gmail.com>
2018-09-21 23:58:07 +0200

    Step 5- Add, Remove, find - Person Service

--- src/main/java/myApp/service/PersonService.java
+++ src/main/java/myApp/service/PersonService.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import myApp.domaine.Person;
 import myApp.repository.PersonDAO;
@@ -35,28 +41,31 @@ public class PersonService {
 		return personne.get();
 	}
 
-	@DeleteMapping(value = "/delete/{id}")
-	public void deletePerson(@PathVariable("id") long id) {
-		personDAO.deleteById(id);
-	}
-
 	@GetMapping("/allPersons")
 	public List<Person> findPeople() {
 		return personDAO.findAll();
 	}
 
-	
-	@PostMapping("/addPerson")
-	public void CreatePerson(Person p) {
+	@PostMapping(value = "/addPerson")
+	public void CreatePerson(@RequestBody Person p) {
 		personDAO.save(p);
 	}
 
-	// public void addPerson(Person p) {
-//		personneDao.save(p);
-//	}
+	@PostMapping(value = "/addPersons")
+	public void CreatePersons(@RequestBody List<Person> listPerson) {
+		for (Person p : listPerson) {
+			personDAO.save(p);
+		}
+	}
 
-//	public void deletePerson(long id) {
-//		personneDao.deleteById(id);
-//
-//	}
+	@DeleteMapping(value = "/delete/{id}")
+	public void deletePerson(@PathVariable("id") long id) {
+		personDAO.deleteById(id);
+	}
+
+	@DeleteMapping("/deleteAll")
+	public void deleteAllPerson() {
+		personDAO.deleteAll();
+	}
+
 }
