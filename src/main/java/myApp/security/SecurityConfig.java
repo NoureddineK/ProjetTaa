c254fd3b71f380417d659a060672f1177b3a6fe7
NoureddineK <noureddine.kadri05@gmail.com>
2018-11-07 08:04:53 +0100

    Refactoring

--- src/main/java/myApp/security/SecurityConfig.java
+++ src/main/java/myApp/security/SecurityConfig.java
@@ -1,45 +1,45 @@
-package myApp.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN", "USER");
-		auth.inMemoryAuthentication().withUser("user").password("1234").roles("USER");
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.formLogin();
-		http.authorizeRequests().antMatchers("/person/*").hasRole("USER");
-
-		http.authorizeRequests().antMatchers("/place/*","/sport/*").hasRole("ADMIN");
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new PasswordEncoder() {
-			@Override
-			public String encode(CharSequence rawPassword) {
-				return rawPassword.toString();
-			}
-
-			@Override
-			public boolean matches(CharSequence rawPassword, String encodedPassword) {
-				return rawPassword.toString().equals(encodedPassword);
-			}
-		};
-	}
-
-}
+//package myApp.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN", "USER");
+//		auth.inMemoryAuthentication().withUser("user").password("1234").roles("USER");
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//
+//		http.formLogin();
+//		http.authorizeRequests().antMatchers("/person/*").hasRole("USER");
+//
+//		http.authorizeRequests().antMatchers("/place/*","/sport/*").hasRole("ADMIN");
+//	}
+//
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new PasswordEncoder() {
+//			@Override
+//			public String encode(CharSequence rawPassword) {
+//				return rawPassword.toString();
+//			}
+//
+//			@Override
+//			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+//				return rawPassword.toString().equals(encodedPassword);
+//			}
+//		};
+//	}
+//
+//}
